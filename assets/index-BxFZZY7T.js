3cd827c6d21550e3e289445cc8c39e4c266b15bd
Pirskiy Maxim <pirskiymi98@gmail.com>
2024-04-30 11:50:56 +0300

    Updates

--- assets/index-BxFZZY7T.js
+++
@@ -1,1 +0,0 @@
-import{j as s,al as r}from"./index-BCM9aTU6.js";const a=()=>s.jsx("div",{className:r.preloader});export{a as P};
---
+++ assets/index-Dcd-epSQ.js
@@ -0,0 +1,1 @@
+import{j as s,al as r}from"./index-R4FHraQG.js";const a=()=>s.jsx("div",{className:r.preloader});export{a as P};
